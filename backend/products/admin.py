af2f5189bcd8935120ca3937ff69d78af30664e6
anass <anaselouanda6@gmail.com>
2026-01-24 23:01:43 +0100

    Ajout modele Category et configuration Admin

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-####
-from .models import Product 
+from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
+# admin.site.register(Product)
+# admin.site.register(Category)
+# 1. Configuration pour les Catégories
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    # MAGIE : Le slug se remplit tout seul quand tu tapes le nom ! 
+    prepopulated_fields = {'slug': ('name',)}
+
+# 2. Configuration pour les Produits
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # On affiche la colonne 'category' pour voir où est rangé chaque produit
+    list_display = ('name', 'category', 'price', 'stock')
+    list_filter = ('category',) # Ajoute un filtre latéral par catégorie
+
--- backend/products/models.py
+++ backend/products/models.py
@@ -2,13 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+
+class Category(models.Model):
+    """Model of category"""
+    name = models.CharField(max_length=100)
+    # Le slug sert à l'URL (ex: /montres-luxe)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     """"Database model for product"""
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE, # Si on supprime la catégorie, le produit disparaît 
+        null=True
+    )
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='produits/%Y/%m/', null=True, blank=True)
     stock = models.IntegerField(default=0)
+    
 
     def get_full_name(self):
         """Retrieve full name of product"""
