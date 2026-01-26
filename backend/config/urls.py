c81c200ae13cbbb30ac110e3a41047746993b683
anass <anaselouanda6@gmail.com>
2026-01-26 18:17:05 +0100

    Ajout des URLs et du Routeur API

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,10 +3,19 @@ from django.urls import path
 ###
 from django.conf import settings
 from django.conf.urls.static import static
+###
+from rest_framework import routers
+from django.urls import include
+from products.views import ProductViewSet, CategoryViewSet
 
+
+router = routers.DefaultRouter()
+router.register('products', ProductViewSet)
+router.register('categories', CategoryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
 
 
